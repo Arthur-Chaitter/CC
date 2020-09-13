0161f6df4342b8b9e062d2cf7e5a00d3f63ed022
Arthur Chaitter <arthur.chaitter47@gmail.com>
2020-09-13 10:04:19 -0300

    sistema bancario

---
+++ Comp.II/SistemaBancario/src/Principal.java
@@ -0,0 +1,15 @@
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Banco banco = new Banco();
+        Agencia agencia = new Agencia();
+        Correntista correntista = new Correntista("Fulano", 1234);
+
+        Conta conta = banco.criarConta(agencia, correntista);
+        System.out.println(conta.getSaldo());
+        conta.receberDepositoEmDinheiro(200);
+        System.out.println(conta.getSaldo());
+        System.out.println(conta.getHistoricoOperacoes()[0]);
+    }
+}
